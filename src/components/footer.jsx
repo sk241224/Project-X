6acd0d1334631deaedc9049690b6c5f6916405dc
Rudransh Pandey <rdrnshpndy@gmail.com>
2025-06-12 23:18:03 +0530

    Update footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,65 +1,352 @@
+import React from "react";
 
-const Footer = () => {
-    return (
-      <footer className="footer">
-        <div className="footer-container">
-          <div className="footer-column">
-            <h3>COLLEGE</h3>
-            <ul>
-              <li>Statewise Colleges</li>
-              <li>Explore All IITs</li>
-              <li>Explore All NITs</li>
-              <li>Explore All IIITs</li>
-              <li>Explore All IISERs</li>
-            </ul>
+function Footer() {
+  return (
+    <footer id="theme-footer">
+      <div className="myft cp-footer-mobile col-sm-12" style={{ marginTop: 0 }}>
+        <div
+          className="footer-links-cont col-xs-12"
+          style={{ marginBottom: "20px", textAlign: "center" }}
+        >
+          <ul className="footer-half-links">
+            <li>
+              <a
+                className="footer-link"
+                href="https://www.collegepravesh.com/about-us/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                About Us
+              </a>
+            </li>
+            <li>
+              <a
+                className="footer-link"
+                href="https://www.collegepravesh.com/privacy-policy/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Privacy Policy
+              </a>
+            </li>
+            <li>
+              <a
+                className="footer-link"
+                href="https://www.collegepravesh.com/terms-of-use/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Terms of Use
+              </a>
+            </li>
+            <li>
+              <a
+                className="footer-link"
+                href="https://www.collegepravesh.com/contact-us/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Contact Us
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div className="myft cp-footer-full col-sm-12 col-md-10 col-lg-10 col-llg-8 col-md-offset-1 col-llg-offset-2">
+        <div className="footer-links-cont col-xs-12 col-sm-8 col-md-5">
+          <div className="footer-header">
+            <span className="footer-text">College</span>
           </div>
-          <div className="footer-column">
-            <h3>EXAM</h3>
-            <ul>
-              <li>JEE (Main)</li>
-              <li>JEE (Advanced)</li>
-              <li>BITSAT</li>
-              <li>MHT-CET</li>
-              <li>UPSEE</li>
-              <li>WBJEE</li>
-            </ul>
+          <ul className="footer-half-links col-xs-12 col-sm-6">
+            <li>
+              <a
+                className="footer-link"
+                href="/college-finder/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Statewise Colleges
+              </a>
+            </li>
+            <li>
+              <a
+                className="footer-link"
+                href="/college-finder/nit-colleges-in-india/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Explore All NITs
+              </a>
+            </li>
+            <li>
+              <a
+                className="footer-link"
+                href="/college-finder/iiit-colleges-in-india/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Explore All IIITs
+              </a>
+            </li>
+            <li>
+              <a
+                className="footer-link"
+                href="/college-finder/iiser-colleges-in-india/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Explore All IISERs
+              </a>
+            </li>
+            <li>
+              <a
+                className="footer-link"
+                href="/college-finder/top-colleges-in-delhi-ncr/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Colleges in Delhi NCR
+              </a>
+            </li>
+            <li>
+              <a
+                className="footer-link"
+                href="/college-finder/top-colleges-in-maharashtra/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Colleges in Maharashtra
+              </a>
+            </li>
+            <li>
+              <a
+                className="footer-link"
+                href="/college-finder/top-colleges-in-karnataka/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Colleges in Karnataka
+              </a>
+            </li>
+            <li>
+              <a
+                className="footer-link"
+                href="/college-finder/top-colleges-in-uttar-pradesh/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Colleges in Uttar Pradesh
+              </a>
+            </li>
+            <li>
+              <a
+                className="footer-link"
+                href="/college-finder/top-colleges-in-andhra-pradesh/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Colleges in Andhra Pradesh
+              </a>
+            </li>
+            <li>
+              <a
+                className="footer-link"
+                href="/college-finder/top-colleges-in-telangana/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Colleges in Telangana
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="footer-links-cont col-xs-12 col-sm-4 col-md-2">
+          <div className="footer-header">
+            <span className="footer-text">Exam</span>
           </div>
-          <div className="footer-column">
-            <h3>TOOLS</h3>
-            <ul>
-              <li>College Finder</li>
-              <li>JEE (Main) Rank Predictor</li>
-              <li>JEE (Main) College Predictor</li>
-              <li>JEE (Advanced) College Predictor</li>
-              <li>BITSAT College Predictor</li>
-              <li>JAC Delhi College Predictor</li>
-            </ul>
+          <ul className="footer-half-links">
+            <li>
+              <a
+                className="footer-link"
+                href="jeemains.html"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                JEE (Mains)
+              </a>
+            </li>
+            <li>
+              <a
+                className="footer-link"
+                href="jeeadvance.html"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                JEE (Advanced)
+              </a>
+            </li>
+            <li>
+              <a
+                className="footer-link"
+                href="bitsat.html"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                BITSAT
+              </a>
+            </li>
+            <li>
+              <a
+                className="footer-link"
+                href="mhtcet.html"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                MHT-CET
+              </a>
+            </li>
+            <li>
+              <a
+                className="footer-link"
+                href="upsee.html"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                UPSEE
+              </a>
+            </li>
+            <li>
+              <a
+                className="footer-link"
+                href="wbjee.html"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                WBJEE
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="footer-links-cont col-xs-12 col-sm-4 col-md-2">
+          <div className="footer-header">
+            <span className="footer-text">Company</span>
           </div>
-          <div className="footer-column">
-            <h3>COMPANY</h3>
-            <ul>
-              <li>About Us</li>
-              <li>Media Kit</li>
-              <li>Privacy Policy</li>
-              <li>Terms of Use</li>
-              <li>Contact Us</li>
-            </ul>
+          <ul className="footer-half-links">
+            <li>
+              <a
+                className="footer-link"
+                href="https://www.collegepravesh.com/about-us/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                About Us
+              </a>
+            </li>
+            <li>
+              <a
+                className="footer-link"
+                href="https://www.collegepravesh.com/privacy-policy/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Privacy Policy
+              </a>
+            </li>
+            <li>
+              <a
+                className="footer-link"
+                href="https://www.collegepravesh.com/terms-of-use/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Terms of Use
+              </a>
+            </li>
+            <li>
+              <a
+                className="footer-link"
+                href="https://www.collegepravesh.com/contact-us/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Contact Us
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div id="footer-widget-area" className="footer-1c">
+        <div id="footer-first" className="footer-widgets-box">
+          <div
+            id="text-html-widget-31"
+            className="text-html-box"
+            style={{ textAlign: "center" }}
+          >
+            <img
+              src="/cpdata/themes/sahifa/images/logo_sizes/Logo_Small_Dark_Transparent_64x64_optimized.png"
+              alt="College Pravesh"
+              draggable="false"
+              height="64"
+              width="64"
+            />
+          </div>
+          <div className="widget social-icons-widget">
+            <div className="social-icons">
+              <a
+                className="ttip-none"
+                title="Facebook"
+                href="https://www.facebook.com/CollegePravesh"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i className="fa fa-facebook"></i>
+              </a>
+              <a
+                className="ttip-none"
+                title="Twitter"
+                href="https://twitter.com/collegepravesh"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i className="fa fa-twitter"></i>
+              </a>
+              <a
+                className="ttip-none"
+                title="LinkedIn"
+                href="https://www.linkedin.com/company/college-pravesh"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i className="fa fa-linkedin"></i>
+              </a>
+              <a
+                className="ttip-none"
+                title="Instagram"
+                href="https://www.instagram.com/college.pravesh/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i className="fa fa-instagram"></i>
+              </a>
+            </div>
+          </div>
+          <div
+            id="text-html-widget-16"
+            className="text-html-box"
+            style={{ textAlign: "center" }}
+          >
+            <div style={{ color: "#aaa" }} id="copyright-txt">
+              College Pravesh © Copyright 2025
+            </div>
           </div>
         </div>
-  
-        <div className="footer-bottom">
-          <div className="social-icons">
-            <i className="fab fa-facebook"></i>
-            <i className="fab fa-twitter"></i>
-            <i className="fab fa-linkedin"></i>
-            <i className="fab fa-instagram"></i>
-          </div>
-          <h1 className="footer-logo">cp</h1>
-          <p>College Pravesh © Copyright 2025</p>
-        </div>
-      </footer>
-    );
-  };
-  
-  export default Footer;
-  
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
